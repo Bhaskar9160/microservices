1260bb2c541c6e2f2e1412efa61c4090d8d4712f
Bhaskar9160 <bhaskar91605@gmail.com>
2023-03-14 14:09:13 +0530

    user service

---
+++ userService/src/main/java/com/akhm/util/EmailDetails.java
@@ -0,0 +1,12 @@
+package com.akhm.util;
+
+import lombok.Data;
+
+@Data
+public class EmailDetails {
+	private String recipient;
+	private String messegeBody;
+	private String subject;
+	private String attachment;
+
+}
